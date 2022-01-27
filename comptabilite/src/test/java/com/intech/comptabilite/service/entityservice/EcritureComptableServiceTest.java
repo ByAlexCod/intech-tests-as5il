c20311920ba5a2f10869edd8751e2f51b37b4653
Alexandre Spitz <a18.spitz@gmail.com>
2022-01-27 12:46:44 +0100

    Fix get by ref is now unit test

--- comptabilite/src/test/java/com/intech/comptabilite/service/entityservice/EcritureComptableServiceTest.java
+++ comptabilite/src/test/java/com/intech/comptabilite/service/entityservice/EcritureComptableServiceTest.java
@@ -3,18 +3,27 @@ package com.intech.comptabilite.service.entityservice;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Date;
+import java.util.Optional;
 import java.util.Random;
 
 import com.intech.comptabilite.model.*;
+import com.intech.comptabilite.repositories.EcritureComptableRepository;
 import com.intech.comptabilite.service.exceptions.NotFoundException;
 import org.apache.commons.lang3.ObjectUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 public class EcritureComptableServiceTest {
+
+    @MockBean EcritureComptableRepository ecritureComptableRepository;
+
 	
 	@Autowired
 	private EcritureComptableService ecritureComptableService;
@@ -96,47 +105,69 @@ public class EcritureComptableServiceTest {
     void testGetByRef() throws NotFoundException
     {
 
-        String REFERENCE = "BQ-2016/00001";
-        CompteComptable compteComptable = saveAndGetRandomCompteComptable();
+        Mockito.when(ecritureComptableRepository.getByReference("BQ-2016/00001"))
+                .thenReturn(java.util.Optional.of(new EcritureComptable()), Optional.empty());
 
+        try {
+            EcritureComptable e = ecritureComptableService.getEcritureComptableByRef("BQ-2016/00001");
+            Assertions.assertNotNull(e);
+        } catch (Exception e) {
+            Assertions.fail("Ecriture comptable should be found");
+        }
 
-        JournalComptable journalComptable;
-        Integer vEcritureId = (new Random()).nextInt(1000);
-        EcritureComptable vEcriture;
-        journalComptable = new JournalComptable();
-        journalComptable.setCode("ABCD");
-        journalComptable.setLibelle("Journal de test");
-        vEcriture = new EcritureComptable();
+        try {
+            EcritureComptable e = ecritureComptableService.getEcritureComptableByRef("BQ-2016/00001");
+            Assertions.fail("Ecriture comptable should not be found");
+        } catch (Exception ignored) {
 
-        vEcriture.setId(vEcritureId);
-        vEcriture.setLibelle("Equilibrée");
-        vEcriture.setDate(new Date());
-        vEcriture.setJournal(journalComptable);
+        }
 
+        //        String REFERENCE = "BQ-2016/00001";
+        //        CompteComptable compteComptable = saveAndGetRandomCompteComptable();
+        //
+        //
+        //        JournalComptable journalComptable;
+        //        Integer vEcritureId = (new Random()).nextInt(1000);
+        //        EcritureComptable vEcriture;
+        //        journalComptable = new JournalComptable();
+        //        journalComptable.setCode("ABCD");
+        //        journalComptable.setLibelle("Journal de test");
+        //        vEcriture = new EcritureComptable();
+        //
+        //        vEcriture.setId(vEcritureId);
+        //        vEcriture.setLibelle("Equilibrée");
+        //        vEcriture.setDate(new Date());
+        //        vEcriture.setJournal(journalComptable);
+        //
+        //
+        //        LigneEcritureComptable ligneEcritureComptableA = this.createLigne(1, "200.50", null);
+        //        ligneEcritureComptableA.setLigneId(new LigneId(vEcritureId, (new Random()).nextInt(1000)));
+        //        ligneEcritureComptableA.setCompteComptable(compteComptable);
+        //
+        //        LigneEcritureComptable ligneEcritureComptableB = this.createLigne(2, "100.50", "33");
+        //        ligneEcritureComptableB.setLigneId(new LigneId(vEcritureId, (new Random()).nextInt(1000)));
+        //        ligneEcritureComptableB.setCompteComptable(compteComptable);
+        //
+        //
+        //        vEcriture.getListLigneEcriture().add(ligneEcritureComptableA);
+        //        vEcriture.getListLigneEcriture().add(ligneEcritureComptableB);
+        //
+        //
+        //        vEcriture.setReference(REFERENCE);
+        //
+        //
+        //        String referenceGiven = vEcriture.getReference();
+        //        Assertions.assertNotNull(referenceGiven);
+        //        Assertions.assertEquals(REFERENCE, referenceGiven);
+        //
+        //        ecritureComptableService.insertEcritureComptable(vEcriture);
+        //        EcritureComptable foundEcriture = ecritureComptableService.getEcritureComptableByRef(REFERENCE);
+        //        Assertions.assertEquals(REFERENCE, foundEcriture.getReference());
+    }
 
-        LigneEcritureComptable ligneEcritureComptableA = this.createLigne(1, "200.50", null);
-        ligneEcritureComptableA.setLigneId(new LigneId(vEcritureId, (new Random()).nextInt(1000)));
-        ligneEcritureComptableA.setCompteComptable(compteComptable);
+    @Test
+    void updateEcritureComptableTest() {
 
-        LigneEcritureComptable ligneEcritureComptableB = this.createLigne(2, "100.50", "33");
-        ligneEcritureComptableB.setLigneId(new LigneId(vEcritureId, (new Random()).nextInt(1000)));
-        ligneEcritureComptableB.setCompteComptable(compteComptable);
-
-
-        vEcriture.getListLigneEcriture().add(ligneEcritureComptableA);
-        vEcriture.getListLigneEcriture().add(ligneEcritureComptableB);
-
-
-        vEcriture.setReference(REFERENCE);
-
-
-        String referenceGiven = vEcriture.getReference();
-        Assertions.assertNotNull(referenceGiven);
-        Assertions.assertEquals(REFERENCE, referenceGiven);
-
-        ecritureComptableService.insertEcritureComptable(vEcriture);
-        EcritureComptable foundEcriture = ecritureComptableService.getEcritureComptableByRef(REFERENCE);
-        Assertions.assertEquals(REFERENCE, foundEcriture.getReference());
     }
 
 }
