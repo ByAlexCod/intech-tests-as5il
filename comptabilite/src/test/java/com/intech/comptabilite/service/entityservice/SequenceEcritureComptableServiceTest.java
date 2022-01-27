bc4207655cdd9086fbe8ce74847dd4f68d06f6d3
Alexandre Spitz <a18.spitz@gmail.com>
2022-01-27 16:33:38 +0100

    SequenceComptableServiceTest

---
+++ comptabilite/src/test/java/com/intech/comptabilite/service/entityservice/SequenceEcritureComptableServiceTest.java
@@ -0,0 +1,107 @@
+package com.intech.comptabilite.service.entityservice;
+import com.intech.comptabilite.model.SequenceEcritureComptable;
+import com.intech.comptabilite.model.SequenceId;
+import com.intech.comptabilite.repositories.SequenceEcritureComptableRepository;
+import com.intech.comptabilite.service.exceptions.NotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@SpringBootTest
+public class SequenceEcritureComptableServiceTest
+{
+
+    @Autowired SequenceEcritureComptableRepository sequenceEcritureComptableRepository;
+    @MockBean SequenceEcritureComptableRepository mockedSequenceRepo;
+
+
+    @Autowired SequenceEcritureComptableService sequenceEcritureComptableService;
+
+    @Test
+    void testUpsertInsert() {
+        SequenceEcritureComptable baseSequence = new SequenceEcritureComptable();
+        baseSequence.setAnnee(2022);
+        baseSequence.setDerniereValeur(1);
+        baseSequence.setJournalCode("AC");
+        Assertions.assertDoesNotThrow(() -> {
+            sequenceEcritureComptableService.upsert(baseSequence);
+        });
+        List<SequenceEcritureComptable> allSequences = new ArrayList<>();
+        sequenceEcritureComptableRepository.findAll().forEach(allSequences::add);
+
+        int countGood = 0;
+
+        for (var sequence: allSequences) {
+            if(sequence.getAnnee() == 2022 && sequence.getDerniereValeur() == 1 && sequence
+                    .getJournalCode().equals("AC")) {
+                countGood ++;
+            }
+        }
+        Assertions.assertEquals(1, countGood);
+    }
+
+    @Test
+    void testUpsertUpdate() {
+        SequenceEcritureComptable baseSequence = new SequenceEcritureComptable();
+        baseSequence.setAnnee(2022);
+        baseSequence.setDerniereValeur(1);
+        baseSequence.setJournalCode("AC");
+        Assertions.assertDoesNotThrow(() -> {
+            sequenceEcritureComptableService.upsert(baseSequence);
+        });
+
+        baseSequence.setDerniereValeur(2);
+        Assertions.assertDoesNotThrow(() -> {
+            sequenceEcritureComptableService.upsert(baseSequence);
+        });
+
+        List<SequenceEcritureComptable> allSequences = new ArrayList<>();
+        sequenceEcritureComptableRepository.findAll().forEach(allSequences::add);
+
+        int countGood = 0;
+
+        for (var sequence: allSequences) {
+            if(sequence.getAnnee() == 2022 && sequence.getDerniereValeur() == 2 && sequence
+                    .getJournalCode().equals("AC")) {
+                countGood ++;
+            }
+        }
+        Assertions.assertEquals(1, countGood);
+    }
+
+    @Test
+    void testUnitGetByDerniereSequenceByAnneAndCode() throws NotFoundException
+    {
+
+        SequenceEcritureComptable testedSequence = new SequenceEcritureComptable();
+        testedSequence.setDerniereValeur(1);
+        testedSequence.setJournalCode("ABC");
+        testedSequence.setAnnee(2001);
+
+        Mockito.when(
+                mockedSequenceRepo.findById(Mockito.any(SequenceId.class))
+        ).thenReturn(
+                Optional.empty()
+        );
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            this.sequenceEcritureComptableService.getDernierValeurByCodeAndAnnee("ABC", 2001);
+        });
+
+        Mockito.when(
+                mockedSequenceRepo.findById(Mockito.any(SequenceId.class))
+        ).thenReturn(
+                Optional.of(testedSequence)
+        );
+
+        Integer lastValue = this.sequenceEcritureComptableService.getDernierValeurByCodeAndAnnee(testedSequence.getJournalCode(), testedSequence.getAnnee());
+        Assertions.assertEquals(1, lastValue);
+
+    }
+}
